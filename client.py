3eb699db2dbbc6830c6e98873dd2059fc3022b9e
brandonsmith-prgrms <sbrandon0108@gmail.com>
2021-12-04 19:59:45 -0500

    client server updates

--- client.py
+++ client.py
@@ -1,2 +1,19 @@
-import socket as sock
-import time
+import socket
+
+class Client:
+    def __init__(self) -> None:
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = "45.79.206.22"
+        self.port = 5555
+        self.color = self.connect()
+    
+    def connect(self) -> int:
+        self.client.connect((self.host, self.port))
+        return int(self.client.recv(2048).decode())
+    
+    def send(self, to_send: str) -> str:
+        try:
+            self.client.send(str.encode(to_send))
+            return self.client.recv(2048).decode()
+        except:
+            print("error sending data")
--- server.py
+++ server.py
@@ -1,15 +1,64 @@
 import socket
-import multiprocessing as mulproc
-import time
+from objects.chess.chess import Chess
+from _thread import *
+import sys
 
-serv = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-_SERVER = "localhost"
-_PORT = 5555
+server = ''
+port = 5555
 
-_SERVER_IP = socket.gethostbyname(_SERVER)
+server_ip = socket.gethostbyname(server)
 
 try:
-    serv.bind((_SERVER, _PORT))
+    s.bind((server, port))
+
 except socket.error as e:
     print(str(e))
+
+s.listen(2)
+print("Waiting for a connection")
+turnNum = 1
+players = [False, False]
+game = Chess()
+
+def threaded_client(conn, num):
+    global turnNum, players, game_board
+    conn.send(str.encode(str(num)))
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode('utf-8')
+            if game.get_turn() and num == 2:
+                continue
+            if num == 1 and not game.get_turn():
+                continue
+            if not data:
+                #do more game ending shit here
+                conn.send(str.encode("Goodbye"))
+            else:
+                print("Recieved: " + reply)
+                if reply != game.board_to_string():
+                    #udpate game_board
+                    game.string_to_board(reply)
+                print("Sending: " + reply)
+
+            conn.sendall(str.encode(game.board_to_string()))
+        except:
+            break
+
+    print("Connection Closed")
+    conn.close()
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+    if not players[0]:
+        players[0] = True
+        start_new_thread(threaded_client, (conn, 1))
+    elif not players[1]:
+        players[1] = True
+        start_new_thread(threaded_client, (conn, 2))
+    else:
+        print("Game is full, rejecting connection")
+        conn.close()
